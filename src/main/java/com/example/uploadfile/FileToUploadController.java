d2e3922a7ab1c71bb729bb0b7ea77356380db0f9
george-ravelly <george.ravelly1@gmail.com>
2022-10-03 18:43:23 -0300

    Parametrização do path para o arquivo

--- src/main/java/com/example/uploadfile/FileToUploadController.java
+++ src/main/java/com/example/uploadfile/FileToUploadController.java
@@ -1,5 +1,6 @@
 package com.example.uploadfile;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,18 @@ import java.util.UUID;
 
 @RestController
 public class FileToUploadController {
+
+    private String pathFile;
+
+    public FileToUploadController (@Value("${app.file.path}") String pathFile){
+        this.pathFile = pathFile;
+    }
+
     @PostMapping("/file")
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile upload) throws IOException {
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile upload){
         FileToUpload arquivo = new FileToUpload();
 //        return arquivo.upload("Code/projetos/files", upload.getOriginalFilename(), upload.getInputStream());
-        var path = "c:\\Code\\projetos\\files\\";
-        var location = path + UUID.randomUUID() + upload.getOriginalFilename();
+        var location = pathFile + UUID.randomUUID() + upload.getOriginalFilename();
         try {
             Files.copy(upload.getInputStream(), Path.of(location), StandardCopyOption.REPLACE_EXISTING);
             return new ResponseEntity<String>("Arquivo carregado com sucesso!", HttpStatus.OK);
